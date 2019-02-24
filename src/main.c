6e4f53ad22df02c9b6933427cab1bbbef3b780b4
Nathan Whitehead <nwhitehe@gmail.com>
2019-02-23 22:42:51 -0800

    Use custom evaluator to try to get return values

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdlib.h>
 
 #include <emscripten.h>
@@ -12,3 +13,13 @@ int main(int argc, char** argv) {
     emscripten_exit_with_live_runtime();
     return 0;
 }
+
+char *CustomEval(char *input) {
+    PyObject *main_module = PyImport_AddModule("__main__");
+    PyObject *globals = PyModule_GetDict(main_module);
+    PyObject *result = PyRun_String(input, Py_eval_input, globals, globals);
+    assert(result);
+    Py_DECREF(result);
+    return "SOME TEXT";
+}
+
