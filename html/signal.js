75c1021c10d6c619eafe1a333905ddac58848b35
Nathan Whitehead <nwhitehe@gmail.com>
2019-03-22 06:29:42 -0700

    Use signals

--- html/signal.js
+++ html/signal.js
@@ -22,7 +22,17 @@ function clear(name) {
 }
 function set(name) {
     Atomics.store(sharedArray, signalMap[name], 1);
+    Atomics.notify(sharedArray, signalMap[name]);
 }
 export function isInterrupt() { return check('interrupt'); }
 export function clearInterrupt() { return clear('interrupt'); }
 export function setInterrupt() { return set('interrupt'); }
+export function isBusy() { return check('busy'); }
+export function clearBusy() { return clear('busy'); }
+export function setBusy() { return set('busy'); }
+export function isLoading() { return check('loading'); }
+export function clearLoading() { return clear('loading'); }
+export function setLoading() { return set('loading'); }
+export function isStarting() { return check('starting'); }
+export function clearStarting() { return clear('starting'); }
+export function setStarting() { return set('starting'); }
