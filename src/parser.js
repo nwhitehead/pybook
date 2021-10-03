7f55eeb4442ca047bf52e0032d23887f2d5deb2a
Nathan Whitehead <nwhitehe@gmail.com>
2021-10-02 20:44:04 -0600

    Rudimentary parsing into cells and pages

--- src/parser.js
+++ src/parser.js
@@ -18,6 +18,7 @@ function isSpecialDelimiter(txt) {
 }
 
 //! Parse a line that is special, return object with info
+//! Throws if delim not legal
 export function parseSpecialDelimiterLine(txt) {
     if (!isSpecialDelimiter(txt)) {
         return null;
@@ -29,14 +30,82 @@ export function parseSpecialDelimiterLine(txt) {
     });
     const options = parts.slice(1);
     const delim = parts[0];
-    if (delim !== '#%' && delim !== '#%%') {
-        throw "Unknown delimiter";
+    let type = '';
+    // Match delim against fixed set of allowed possibilities
+    if (delim === '#%') {
+        type = 'Code';
+    } else {
+        if (delim === '#%%') {
+            type = 'Markdown';
+        } else {
+            if (delim === '#%page') {
+                type = 'Page';
+            } else {
+                throw 'Unknown delimiter';
+            }
+        }
     }
-    let type = (delim === '#%' ? 'Markdown' : (delim === '#%%' ? 'Code' : 'Unknown'));
     return { type, options };
 }
 
 export function parse(text) {
+    // Strategy is to accumulate lines into latest item, and items into latest page, etc.
+    let pages = []; // Array of pages
+    let page = []; // Array of items
+    let item = []; // Array of lines
+    let currentType = 'start'; // Assume we start in unknown type
+    let currentOptions = [];
+    function finishItem() {
+        if (currentType === 'start') {
+            // Ignore things before first delim
+            item = [];
+            currentType = '';
+            currentOptions = [];
+            return;
+        }
+        if (currentType === '') {
+            throw 'No item to finish';
+        }
+        const itemStr = item.join('\n');
+        page.push({ type:currentType, options:currentOptions, data:itemStr });
+        item = [];
+        currentType = '';
+        currentOptions = [];
+    }
+    function finishPage() {
+        finishItem();
+        if (page.length === 0) {
+            throw 'No page to finish';
+        }
+        pages.push(page);
+        page = [];
+    }
     const lines = splitByLines(text);
-    return lines;
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        if (isSpecialDelimiter(line)) {
+            const delim = parseSpecialDelimiterLine(line);
+            if (delim.type === 'Markdown') {
+                finishItem();
+                currentType = delim.type;
+                currentOptions = delim.options;
+            } else {
+                if (delim.type === 'Code') {
+                    finishItem();
+                    currentType = delim.type;
+                    currentOptions = delim.options;
+                } else {
+                    if (delim.type === 'Page') {
+                        finishPage();
+                    } else {
+                        throw 'Unexpected delimiter';
+                    }
+                }
+            }
+        } else {
+            item.push(line);
+        }
+    }
+    finishPage();
+    return pages;
 }
--- src/parser.test.js
+++ src/parser.test.js
@@ -2,13 +2,19 @@ import { parseSpecialDelimiterLine,
          parse } from './parser.js';
 
 test('parseSpecialDelimiterLine1', () => {
-    expect(parseSpecialDelimiterLine('#%')).toStrictEqual(
-        { 'type': 'Markdown', options: []});
     expect(parseSpecialDelimiterLine('#%%')).toStrictEqual(
+        { 'type': 'Markdown', options: []});
+    expect(parseSpecialDelimiterLine('#%')).toStrictEqual(
         { 'type': 'Code', options: []});
     expect(() => parseSpecialDelimiterLine('#%md')).toThrow('Unknown delimiter');
-    expect(parseSpecialDelimiterLine('#%% noexec')).toStrictEqual(
+    expect(parseSpecialDelimiterLine('#% noexec')).toStrictEqual(
         { 'type': 'Code', options: ['noexec']});
-    expect(parseSpecialDelimiterLine('#%% hidden noexec')).toStrictEqual(
+    expect(parseSpecialDelimiterLine('#% hidden noexec')).toStrictEqual(
         { 'type': 'Code', options: ['hidden', 'noexec']});
+});
+
+test('parser', () => {
+    const tst = '#%% hidden\n# Headline\n#%\nprint(42)\n';
+    const res = parse(tst);
+    console.log(res);
 });
