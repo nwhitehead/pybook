e250e60186748f05cd47e2cdd638a2c4c09204b7
Nathan Whitehead <nwhitehe@gmail.com>
2021-11-06 21:03:51 -0600

    Move newPythonWorker contents into python.js to clean up code

--- src/python.js
+++ src/python.js
@@ -1,10 +1,46 @@
-import { signalMap,
-         isBusy,
-         isStarting, setStarting,
-         setInterrupt, clearInterrupt
+import { signalMap, setStarting, clearInterrupt,
+         sharedArray, sharedInputArray, INPUT_BUFFER_SIZE
        } from './signal.js';
 
-import { newPythonWorker } from './newPythonWorker.js';
+// Spawn the web worker thread and configure it
+function newPythonWorker() {
+    const config = {
+        absurl: document.location.protocol + '//' + document.location.host,
+        sharedArray: sharedArray,
+        sharedInputArray: sharedInputArray,
+        signalMap: signalMap,
+        INPUT_BUFFER_SIZE: INPUT_BUFFER_SIZE,
+    };
+
+    let worker = new Worker('worker.js');
+    worker.postMessage(config);
+
+    worker.send = function(msg) {
+        worker.postMessage(msg);
+        return worker;
+    };
+    worker.on = function(type, handler) {
+        if (type === 'message') {
+            worker.onmessage = function(e) {
+                handler(e.data);
+            };
+            return worker;
+        }
+        if (type === 'error') {
+            worker.onerror = handler;
+            console.log('error in spawned thread');
+            return worker;
+        }
+        if (type === 'terminate') {
+            worker._onterminate = handler;
+        }
+        throw 'Unknown message type ' + type;
+    };
+
+    // Set starting, worker will clear when ready
+    setStarting();
+    return worker;
+}
 
 //!
 //! \brief Find one handler in dict with fallback opts
