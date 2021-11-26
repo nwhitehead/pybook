e8e67ffb5dd105eb54a2d9dcd9965ae102a301be
Nathan Whitehead <nwhitehe@gmail.com>
2021-11-26 08:22:27 -0700

    Trying to make single file components Need a build flow grrrrrr

---
+++ src/components/Status.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div class="status">
+        <span class="material-icons" v-if="value=='Initializing'">cloud</span>
+        <span class="material-icons" v-if="value=='Ready'">done</span>
+        <span class="material-icons" v-if="value=='Working'">cloud</span>
+        <span class="material-icons" v-if="value=='Interrupt'">report</span>
+        <p>{{ value }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['value']
+}
+</script>
