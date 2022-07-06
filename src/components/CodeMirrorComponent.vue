af4b96b54944757b7b15e9dcd54064c5dd9a1a07
Nathan Whitehead <nwhitehe@gmail.com>
2022-07-06 09:32:16 -0600

    Using codemirror npm package

--- src/components/CodeMirrorComponent.vue
+++ src/components/CodeMirrorComponent.vue
@@ -1,77 +1,87 @@
 <template>
-    <div class="cellinput">
-        <textarea></textarea>
-    </div>
+  <div class="cellinput">
+    <textarea></textarea>
+  </div>
 </template>
 
 <script>
+import { EditorState, Compartment } from "@codemirror/state";
+import { EditorView, keymap } from "@codemirror/view";
+import { defaultKeymap } from "@codemirror/commands";
+import { python } from "@codemirror/lang-python";
+import { markdown } from "@codemirror/lang-markdown";
 
-import CodeMirror from 'codemirror';
-import EventBus from './EventBus.js';
+import EventBus from "./EventBus.js";
 
 export default {
-    props: ['options', 'value', 'id'],
-    mounted: function() {
-        var self = this;
-        var elem = self.$el.querySelector('textarea');
-        self.editor = CodeMirror.fromTextArea(elem, self.options);
-        // Vue global variable $globalCMList keeps track of all instances of CodeMirror (so we can shuffle them easily on drag)
-        if (self.$globalCMList === undefined) {
-            self.$globalCMList = [];
-        }
-        self.$globalCMList.push(self.editor);
-        self.editor.getWrapperElement().classList.add('cellinput');
-        self.editor.setValue(self.value || '');
-        self.editor.on('change', function(cm) {
-            self.$emit('update:value', cm.getValue());
-        });
-        self.editor.on('focus', function(cm) {
-            EventBus.$emit('cmfocus', { id:self.id });
-        });
-        self.editor.setOption('extraKeys', {
-            // Remove default actions for commands handled at higher app level
-            'Ctrl-Up': function(cm) {},
-            'Ctrl-Down': function(cm) {},
-            'Ctrl-Left': function(cm) {},
-            'Ctrl-Right': function(cm) {},
-            'Esc': function(cm) {},
-            'Ctrl-Enter': function(cm) {},
-            'Shift-Enter': function(cm) {},
-            'Alt-Enter': function(cm) {},
-            'Ctrl-C': function(cm) {},
-            'Ctrl-K': function(cm) {},
-            'Ctrl-I': function(cm) {},
-            // Make pressing "tab" key insert spaces to correct indent level
-            'Tab': function(cm) {
-                let spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
-                cm.replaceSelection(spaces);
-            },
-        });
-    },
-    watch: {
-        value: function(v) {
-            var currentValue = this.editor.getValue();
-            if (currentValue !== v) {
-                this.editor.setValue(v);
-            }
-        },
-        options: function(opts) {
-            // If options changes, need to set each one in editor
-            Object.entries(opts).forEach(([k, v]) => {
-                this.editor.setOption(k, v);
-            });
-        }
-    },
-    methods: {
-        focus () {
-            this.editor.focus();
-            return true;
-        },
-        blur () {
-            this.editor.getInputField().blur();
-            return true;
-        }
+  props: ["options", "value", "id"],
+  mounted: function () {
+    var self = this;
+    var elem = self.$el.querySelector("textarea");
+    self.startState = EditorState.create({
+      doc: "",
+      extensions: [keymap.of(defaultKeymap)],
+    });
+    self.editor = new EditorView({
+      state: startState,
+      parent: elem,
+    });
+    // Vue global variable $globalCMList keeps track of all instances of CodeMirror (so we can shuffle them easily on drag)
+    if (self.$globalCMList === undefined) {
+      self.$globalCMList = [];
     }
-}
-
+    self.$globalCMList.push(self.editor);
+    self.editor.getWrapperElement().classList.add("cellinput");
+    self.editor.setValue(self.value || "");
+    self.editor.on("change", function (cm) {
+      self.$emit("update:value", cm.getValue());
+    });
+    self.editor.on("focus", function (cm) {
+      EventBus.$emit("cmfocus", { id: self.id });
+    });
+    self.editor.setOption("extraKeys", {
+      // Remove default actions for commands handled at higher app level
+      "Ctrl-Up": function (cm) {},
+      "Ctrl-Down": function (cm) {},
+      "Ctrl-Left": function (cm) {},
+      "Ctrl-Right": function (cm) {},
+      Esc: function (cm) {},
+      "Ctrl-Enter": function (cm) {},
+      "Shift-Enter": function (cm) {},
+      "Alt-Enter": function (cm) {},
+      "Ctrl-C": function (cm) {},
+      "Ctrl-K": function (cm) {},
+      "Ctrl-I": function (cm) {},
+      // Make pressing "tab" key insert spaces to correct indent level
+      Tab: function (cm) {
+        let spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
+        cm.replaceSelection(spaces);
+      },
+    });
+  },
+  watch: {
+    value: function (v) {
+      var currentValue = this.editor.getValue();
+      if (currentValue !== v) {
+        this.editor.setValue(v);
+      }
+    },
+    options: function (opts) {
+      // If options changes, need to set each one in editor
+      Object.entries(opts).forEach(([k, v]) => {
+        this.editor.setOption(k, v);
+      });
+    },
+  },
+  methods: {
+    focus() {
+      this.editor.focus();
+      return true;
+    },
+    blur() {
+      this.editor.getInputField().blur();
+      return true;
+    },
+  },
+};
 </script>
