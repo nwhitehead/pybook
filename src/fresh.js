324cd2a03e9d5880e886fdf865b7ed14927b9bf8
Nathan Whitehead <nwhitehe@gmail.com>
2022-07-15 08:56:51 -0600

    Working on notebook store

--- src/fresh.js
+++ src/fresh.js
@@ -1,10 +1,25 @@
+//!
+//! Fresh identifier generation
+//!
+
 let globalId = 0;
 
-//!
-//! Generate new identifier guaranteed to be unique across all calls
-//!
+//! Generate new stringn identifier guaranteed to be unique across all calls
 export function freshName() {
     const name = 'Id ' + globalId;
     globalId++;
     return name;
 }
+
+//! Generate new identifier number guaranteed to be unique across all cells
+export function freshId() {
+    const id = globalId;
+    globalId++;
+    return id;
+}
+
+//! Update global index to avoid overlapping with index n;
+export function updateGlobalIndex(n) {
+    globalId = globalId <= n ? n + 1 : globalId;
+}
+
--- src/stores/notebook.js
+++ src/stores/notebook.js
@@ -1,7 +1,11 @@
 import { defineStore } from 'pinia';
+import { freshId } from '../fresh.js';
 
+//! Create a default blank cell
+//!
 function newCell() {
   return {
+    id: freshId(),
     source: '',
     cell_type: 'code',
     language: 'python',
@@ -10,17 +14,35 @@ function newCell() {
   };
 }
 
+//! Create a default new page with one cell
+//!
 function newPage() {
   return [ newCell() ];
 }
 
+// A notebook is an array of pages
+let state = [ newPage() ];
+
 export const useNotebook = defineStore('notebook', {
     state: () => {
-      return { count: 0 }
+      return {
+        cells: [ newPage() ],
+      };
+    },
+    getters: {
+      cell: (state, page, id) => {
+        if (page < 0 || page >= state.cells.length) {
+          throw 'Could not get requested page';
+        }
+        const nbpage = state.cells[page];
+        for (let i = 0; i < nbpage.length; i++) {
+          if (nbpage[i].id === id) {
+            return nbpage[i];
+          }
+        }
+        throw 'Could not get requested cell';
+      },
     },
     actions: {
-      increment () {
-        this.count++
-      },
     },
   });
