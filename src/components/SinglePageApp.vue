0f3b10eb8cd230887bcb19a79d655037b6a4cb35
Nathan Whitehead <nwhitehe@gmail.com>
2022-07-31 08:37:27 -0600

    Add axios support, start doing axios queries to server for chooser

--- src/components/SinglePageApp.vue
+++ src/components/SinglePageApp.vue
@@ -8,26 +8,37 @@
     <div>
         <p>Hello to the Single Page App</p>
     </div>
-    <Chooser v-model="choices" :fields="fields" />
+    <Chooser v-model="choices" :fields="fields" @click="handleChooserClick" />
     <Notebook v-model="nbstate" />
 </template>
 
 <script setup>
 
-import { reactive } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import Notebook from "./Notebook.vue";
 import Chooser from "./Chooser.vue";
 import { blankState } from "../notebook.js";
+import axios from 'axios';
 
-let choices = [
-    {name:'Document 1'},
-    {name:'Second Doc'},
-];
+onMounted(async () => {
+    console.log('SinglePageApp component created');
+    const res = await axios.get(`/notebooks`);
+    console.log('Got notebooks', res.data);
+    choices.value = res.data;
+});
+
+let choices = ref([]);
 
 let fields = [
     {name:'name', title:'Name'},
 ];
 
 let nbstate = reactive(blankState);
+
+async function handleChooserClick (item) {
+    console.log('Handle chooser click for identifier ', item.identifier);
+    const res = await axios.get(`/notebook/${item.identifier}`);
+    console.log('Got notebook ', res.data);
+}
 
 </script>
