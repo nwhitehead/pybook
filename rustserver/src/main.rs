b3432e45a43503133ffb2462485f15ad005deb7b
Nathan Whitehead <nwhitehe@gmail.com>
2022-10-11 15:38:22 -0600

    Trying to get headers working in rocket, not working

--- rustserver/src/main.rs
+++ rustserver/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+
+#[macro_use] extern crate rocket;
+
+use rocket::http::{Header, ContentType};
+
+#[derive(Responder)]
+#[response()]
+struct JavaScriptApplication {
+    text: &'static str
+}
+
+#[rocket::get("/")]
+fn index() -> JavaScriptApplication {
+    JavaScriptApplication { text: "Hello, world!\n" }
+}
+
+struct Wrapping<R>(R);
+
+// struct JavaScriptFile(&'static str);
+
+// impl rocket::response::Responder for JavaScriptFile {}
+
+#[rocket::launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", rocket::routes![index])
+        .mount("/static", rocket::fs::FileServer::from(".."))
 }
