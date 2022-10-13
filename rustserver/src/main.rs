12b35509e4189a3b8b0305c0533531141d38537a
Nathan Whitehead <nwhitehe@gmail.com>
2022-10-13 14:48:55 -0600

    Actually do some selects

--- rustserver/src/main.rs
+++ rustserver/src/main.rs
@@ -5,9 +5,18 @@ use actix_files as fs;
 const DB_FILENAME : &'static str = "../build/pybookdb.sql";
 const NOTEBOOK_DIR : &'static str = "../notebooks";
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+struct Database {
+    connection : sqlite::Connection,
+}
+
+#[derive(Debug)]
+struct CustomError {}
+
+#[get("/notebooks")]
+async fn notebooks(data: web::Data<std::sync::Mutex<Database>>) -> impl Responder {
+    let db = data.lock().unwrap();
+    let ids = db.get_notebooks().unwrap();
+    HttpResponse::Ok().body(format!("Notebook ids are {:?}\n", ids))
 }
 
 #[post("/echo")]
@@ -15,63 +24,79 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
+impl Database {
 
-fn setup_db() {
-    let connection = sqlite::open(DB_FILENAME).unwrap();
-    // Create the tables if they don't exist
-    connection.execute(
-        "
-        CREATE TABLE IF NOT EXiSTS documents (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, current TEXT);
-        CREATE TABLE IF NOT EXISTS history (id INTEGER, datetime TEXT, data TEXT);
-        "
-    ).unwrap();
-    // Add the files from the notebooks directory if they aren't already in the db
-    for entry in std::fs::read_dir(NOTEBOOK_DIR).unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        let filename = path.file_name().unwrap().to_str().unwrap();
-        println!("File considered is {:}", filename);
-        // Look for the filename in the documents
-        let mut stmt = connection.prepare(
-            "SELECT * FROM documents WHERE name = ?1"
-        ).unwrap()
-        .bind(1, filename)
-        .unwrap();
-        if stmt.next().unwrap() == sqlite::State::Row {
-            println!("Found filename={}", stmt.read::<String>(1).unwrap());
-        } else {
-            // Not found, so read file and add contents to documents
-            let contents = std::fs::read_to_string(entry.path()).unwrap();
-            connection.prepare(
-                "INSERT INTO documents (name, current) VALUES (?1, ?2)"
+    fn new() -> Database {
+        let connection = sqlite::open(DB_FILENAME).unwrap();
+        // Create the tables if they don't exist
+        connection.execute(
+            "
+            CREATE TABLE IF NOT EXiSTS documents (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, current TEXT);
+            CREATE TABLE IF NOT EXISTS history (id INTEGER, datetime TEXT, data TEXT);
+            "
+        ).unwrap();
+        // Add the files from the notebooks directory if they aren't already in the db
+        for entry in std::fs::read_dir(NOTEBOOK_DIR).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            let filename = path.file_name().unwrap().to_str().unwrap();
+            // Look for the filename in the documents
+            let mut stmt = connection.prepare(
+                "SELECT * FROM documents WHERE name = ?1"
             ).unwrap()
             .bind(1, filename)
-            .unwrap()
-            .bind(2, contents.as_str())
-            .unwrap()
-            .next()
             .unwrap();
+            if stmt.next().unwrap() != sqlite::State::Row {
+                // Not found, so read file and add contents to documents
+                let contents = std::fs::read_to_string(entry.path()).unwrap();
+                connection.prepare(
+                    "INSERT INTO documents (name, current) VALUES (?1, ?2)"
+                ).unwrap()
+                .bind(1, filename)
+                .unwrap()
+                .bind(2, contents.as_str())
+                .unwrap()
+                .next()
+                .unwrap();
+            }
         }
+        return Database {
+            connection: connection
+        };
+    }
+    fn get_notebooks(&self) -> Result<std::vec::Vec<i64>, CustomError> {
+        let connection = &self.connection;
+        let mut stmt = connection.prepare(
+            "SELECT * FROM documents"
+        ).unwrap();
+        let mut result : std::vec::Vec<i64> = std::vec::Vec::new();
+        while stmt.next().unwrap() == sqlite::State::Row {
+            let id = stmt.read::<i64>(0).unwrap();
+            result.push(id);
+        }
+        return Ok(result)/* Err(CustomError {}) */
     }
 }
 
+// fn get_notebooks() -> Result<std::vec::Vec<i64>, Box<dyn std::error::Error>> {
+//     return Err()
+// }
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    setup_db();
+    let db = Database::new();
+    let data = web::Data::new(std::sync::Mutex::new(db));
+
     println!("Starting web server");
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .app_data(data.clone())
             .wrap(middleware::DefaultHeaders::new()
                 .add(("Cross-Origin-Embedder-Policy", "require-corp"))
                 .add(("Cross-Origin-Opener-Policy", "same-origin"))
             )
             .service(fs::Files::new("/static", "..", ))
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(notebooks)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
