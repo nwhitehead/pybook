34b6dfca97d2c503e57f17d2a6f8c76151539ce1
Nathan Whitehead <nwhitehe@gmail.com>
2022-10-30 19:34:07 -0600

    Add test page for console input UI test.

---
+++ src/components/TestApp.vue
@@ -0,0 +1,23 @@
+//!
+//! TestApp
+//!
+//! A Vue component representing the top level single-page test app.
+//!
+
+<template>
+    <ConsoleInput v-model="entry" :options="options" />
+</template>
+
+<script setup>
+
+import ConsoleInput from './ConsoleInput.vue';
+
+import { ref } from 'vue';
+
+let entry = ref('');
+
+let options = {
+    type:'python',
+};
+
+</script>
--- src/main_console.js
+++ src/main_console.js
@@ -1,9 +1,9 @@
 // main_console.js
-import SinglePageApp from './components/ConsoleInput.vue';
+import App from './components/TestApp.vue';
 import VueCodemirror from 'vue-codemirror';
 import { createApp } from 'vue';
 
-const app = createApp(SinglePageApp);
+const app = createApp(App);
 app.use(VueCodemirror, {
     // Set global Codemirror extension default list to empty
     // Otherwise we always get basic-setup extensions and cannot turn them off in components
