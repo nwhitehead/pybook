0e71f7ccd8c7c3ef809698b03996951212927ddc
Nathan Whitehead <nwhitehe@gmail.com>
2022-11-10 15:52:52 -0700

    Working on nav bar.

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -23,16 +23,18 @@
 
     <div id="navbarBasicExample" class="navbar-menu">
         <div class="navbar-start">
-            <a class="navbar-item">Home</a>
-            <a class="navbar-item">Hints</a>
+            <a class="navbar-item" href="/">Home</a>
+            <a class="navbar-item" href="/console.html">Console</a>
+            <a class="navbar-item" href="/code.html">Editor</a>
+            <a class="navbar-item" href="/hint.html">Usage</a>
             <div class="navbar-item has-dropdown is-hoverable">
                 <a class="navbar-link">More</a>
                 <div class="navbar-dropdown">
-                    <a class="navbar-item">About</a>
-                    <a class="navbar-item">Roadmap</a>
-                    <a class="navbar-item">Contact</a>
+                    <a class="navbar-item" href="/about.html">About</a>
+                    <a class="navbar-item" href="/roadmap.html">Roadmap</a>
+                    <a class="navbar-item" href="/contact.html">Contact</a>
                     <hr class="navbar-divider">
-                    <a class="navbar-item">Report an issue</a>
+                    <a class="navbar-item" href="/feedback.html">Report an issue</a>
                 </div>
             </div>
         </div>
@@ -57,9 +59,33 @@ nav.navbar.dark img {
     filter: invert(1) hue-rotate(180deg);
 }
 nav.navbar.dark.has-shadow {
+    /* Fix shadow in dark mode to be darker */
     box-shadow: 0 2px 0 0 #444;
 }
-
+nav.navbar.dark .navbar-item {
+    color: #ddd;
+}
+nav.navbar.dark .navbar-item:hover {
+    background-color: #222;
+    color: #ddd;
+}
+nav.navbar.dark .navbar-link {
+    background-color: #000;
+    color: #ddd;
+}
+nav.navbar.dark .navbar-link:hover {
+    background-color: #222;
+    color: #ddd;
+}
+nav.navbar.dark .navbar-dropdown
+{
+    background-color: #111;
+    color: #ddd;
+}
+nav.navbar.dark .navbar-item.has-dropdown {
+    background-color: #000;
+    color: #ddd;
+}
 </style>
 
 <script setup>
