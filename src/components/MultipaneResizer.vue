c9394334cd8a6cbe5487f42e72d41e4ed5aed933
Nathan Whitehead <nwhitehe@gmail.com>
2022-11-18 10:51:02 -0700

    Oops forgot a file.

---
+++ src/components/MultipaneResizer.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="multipane-resizer">
+        <span :class="resizerClasses()"><span class="material-icons">drag_handle</span></span>
+    </div>
+</template>
+
+<style>
+span.resizer {
+    color: var(--grey-light);
+    user-select: none;
+    pointer-events: none;
+    position: absolute;
+}
+span.resizer.vertical {
+    transform: rotate(90deg);
+    left: -5px;
+    top: 50%;
+}
+span.resizer.horizontal {
+    left: 50%;
+    top: -9px;
+}
+</style>
+
+<script setup>
+
+const props = defineProps([ 'layout' ]);
+
+function resizerClasses() {
+    return {
+        resizer:true,
+        vertical: props.layout === 'vertical',
+        horizontal: props.layout === 'horizontal',
+    };
+}
+</script>
