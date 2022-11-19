4c345d010414c3ddb753feecd63ad697ceb3d648
Nathan Whitehead <nwhitehe@gmail.com>
2022-11-18 21:51:46 -0700

    Add italics to syntax highlighting.

--- src/components/codemirrorLightTheme.js
+++ src/components/codemirrorLightTheme.js
@@ -3,24 +3,18 @@ import { EditorView } from '@codemirror/view';
 import { HighlightStyle, syntaxHighlighting, defaultHighlightStyle } from '@codemirror/language';
 import { tags } from '@lezer/highlight';
 
-/*
-globalThis.hls = defaultHighlightStyle;
-console.log(defaultHighlightStyle);
-*/
-
 /**
 The highlighting style
 */
-//const consoleLightHighlightStyle = HighlightStyle.define(defaultHighlightStyle.specs);
+const consoleLightHighlightStyle = HighlightStyle.define(defaultHighlightStyle.specs.concat(
+    [
+        { tag: [tags.meta, tags.comment],
+            color: '#940',
+            fontStyle: 'italic' 
+        },
+    ]
+));
 
-
-//     [
-//     { tag: [tags.meta, tags.comment],
-//         color: '#888',
-//         fontStyle: 'italic' 
-//     },
-// ]);
-
-const consoleLightExtension = [syntaxHighlighting(/*consoleLightHighlightStyle*/defaultHighlightStyle)];
+const consoleLightExtension = [syntaxHighlighting(consoleLightHighlightStyle)];
 
 export { consoleLightExtension };
